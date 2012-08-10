13caa72596488ddb916e10edd1b632c73abf6985
Tom Marshall <tdm.code@gmail.com>
2012-08-10 13:34:06 -0500

    Initial commit.

---
+++ VS660-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += \
+	vendor/lge/VS660/proprietary/lib/libcm.so:system/lib/libcm.so \
+	vendor/lge/VS660/proprietary/lib/libnv.so:system/lib/libnv.so \
+	vendor/lge/VS660/proprietary/lib/libnv.so:obj/lib/libnv.so \
+	vendor/lge/VS660/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
+	vendor/lge/VS660/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
+	vendor/lge/VS660/proprietary/lib/libril.so:system/lib/libril.so \
+	vendor/lge/VS660/proprietary/lib/libril.so:obj/lib/libril.so \
+	vendor/lge/VS660/proprietary/lib/libsnd.so:system/lib/libsnd.so \
+	vendor/lge/VS660/proprietary/system/bin/ami304d:system/bin/ami304d \
+	vendor/lge/VS660/proprietary/system/bin/rild:system/bin/rild
---
+++ VS660-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/lge/VS660/VS660-vendor-blobs.mk)
